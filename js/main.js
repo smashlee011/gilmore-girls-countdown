3ab8f424521e86ca46f76c2df1bf247e5f07d198
Ashlee Phillips <ashlee.phillips201@gmail.com>
2016-11-13 14:31:29 -0800

    added JavaScript countdown clock to the page

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function (){
+
+	function getTimeRemaining(endtime) {
+	  var t = Date.parse(endtime) - Date.parse(new Date());
+	  var seconds = Math.floor((t / 1000) % 60);
+	  var minutes = Math.floor((t / 1000 / 60) % 60);
+	  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	  return {
+	    'total': t,
+	    'days': days,
+	    'hours': hours,
+	    'minutes': minutes,
+	    'seconds': seconds
+	  };
+	}
+
+	function initializeClock(id, endtime) {
+	  var clock = document.getElementById(id);
+	  var daysSpan = clock.querySelector('.days');
+	  var hoursSpan = clock.querySelector('.hours');
+	  var minutesSpan = clock.querySelector('.minutes');
+	  var secondsSpan = clock.querySelector('.seconds');
+
+	  function updateClock() {
+	    var t = getTimeRemaining(endtime);
+
+	    daysSpan.innerHTML = t.days;
+	    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+	    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+	    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+	    if (t.total <= 0) {
+	      clearInterval(timeinterval);
+	    }
+	  }
+
+	  updateClock();
+	  var timeinterval = setInterval(updateClock, 1000);
+	}
+
+	function getGMT() {
+		var currentTime = new Date();
+		var currentTimezone = currentTime.getTimezoneOffset();
+		currentTimezone = (currentTimezone/60) * -1;
+		var gmt = "";
+		if (currentTimezone !== 0) {
+		  gmt += currentTimezone > 0 ? ' +0' : ' ';
+		  gmt += currentTimezone + "00";
+		}
+		return gmt;
+	}
+	
+	var newGMT = getGMT();
+	var deadline = new Date("Fri, 25 Nov 2016 00:01:00 " + newGMT);
+	initializeClock('clockdiv', deadline);
+
+
+
+});
